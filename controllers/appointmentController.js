eca330a22c71c0b99cad562063f0d2c41f491688
DhRuva-1509 <dhruvasp.ca@gmail.com>
2025-07-02 19:20:06 -0300

    Added 404 statusCode

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -32,19 +32,24 @@ exports.createAppointment = async (req, res) => {
 };
 
 exports.getAppointments = async (req, res) => {
-    try {
-      const { patientId } = req.query;
-  
-      if (!patientId) {
-        return res.status(400).json({ message: 'patientId query param is required.' });
-      }
-  
-      const appointments = await Appointment.find({ patientId }).select('-__v');
-  
-      res.status(200).json({ appointments });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error.' });
+  try {
+    const { patientId } = req.query;
+
+    if (!patientId) {
+      return res.status(400).json({ message: 'patientId query param is required.' });
     }
-  };
+
+    const appointments = await Appointment.find({ patientId }).select('-__v');
+
+    if (appointments.length === 0) {
+      return res.status(404).json({ message: 'No appointments found for this patient.' });
+    }
+
+    res.status(200).json({ appointments });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error.' });
+  }
+};
    
